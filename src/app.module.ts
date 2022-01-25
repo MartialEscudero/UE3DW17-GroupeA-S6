3365e9656df0b525c40f23fc78a895543b274a97
Martial Escudero <martial.escudero@gmail.com>
2022-01-25 12:58:22 +0000

    create base app + add update

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+    }),
+    MongooseModule.forRoot(
+      'mongodb+srv://'
+      + process.env.MONGO_USER + ':'
+      + process.env.MONGO_PASSWORD + '@'
+      + process.env.MONGO_CLUSTER + '.mongodb.net/'
+      + process.env.MONGO_DATABASE_NAME + '?retryWrites=true&w=majority'
+    ),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
